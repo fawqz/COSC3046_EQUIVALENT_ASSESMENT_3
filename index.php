6c24b3f6ab033888df6c94285f9a9a50955d6bc6
fawqz <131230313+fawqz@users.noreply.github.com>
2023-12-03 23:47:46 +1100

    testing title

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Apparel</title>
+    <title>Testing Title</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="styles.css">
     <script src="scripts.js" defer></script>
